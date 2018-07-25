bfdc605cc6ef5027bc7cf92c786570919f21c27f
ygli3 <ygli3@iflytek.com>
2018-07-25 15:29:32 +0800

    add high attribute and updateUser method

--- src/main/java/com/ygli3/entity/User.java
+++ src/main/java/com/ygli3/entity/User.java
@@ -7,6 +7,7 @@ public class User implements Serializable{
     private String name;
     private Integer age;
     private Float weight;
+    private Float high;
 
     public String getName() {
         return name;
@@ -32,4 +33,11 @@ public class User implements Serializable{
         this.weight = weight;
     }
 
+    public Float getHigh() {
+        return high;
+    }
+
+    public void setHigh(Float high) {
+        this.high = high;
+    }
 }
