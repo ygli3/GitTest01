f3790000aa2d0d58f1a6959c6f7eeb361c338c52
ygli3 <ygli3@iflytek.com>
2018-07-25 19:19:51 +0800

    add code to UserDao by B

--- src/main/java/com/ygli3/dao/UserDao.java
+++ src/main/java/com/ygli3/dao/UserDao.java
@@ -23,6 +23,7 @@ public class UserDao {
     public void deleteUser(Integer id){
         System.out.println("B开发者添加的代码。。。。");
         System.out.println("删除User的方法啊！");
+        System.out.println("B开发者添加的代码。。。。");
     }
 
 
